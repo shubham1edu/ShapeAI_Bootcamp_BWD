875538a288379d3b224a9055aa6c0d03096e1b6f
Shubham Tiwari <84079416+shubham1edu@users.noreply.github.com>
2021-06-05 14:00:52 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  var Year = new Date().getFullYear();
+  return (
+    <footer>
+      <p> Copyright @ {Year}</p>
+    </footer>
+  );
+}
+
+export default Footer;
